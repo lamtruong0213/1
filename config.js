bb7d8c747888c96ef69ecb52a707bb23d59e89fb
BELIAL <74252771+lamtruong0213@users.noreply.github.com>
2021-11-10 22:48:44 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng sinh nhật';
+const titleCard = 'Gửi tình yêu của anh';
+const contentCard = 'Chúc honey của anh ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
